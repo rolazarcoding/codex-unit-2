0e0a335ed4bc16b2f1d8bf1ef42d586aaa512b35
Rolazar <rolazarcoding@gmail.com>
2026-04-23 17:59:01 -0700

    finished level 7

--- lesson-04-js-objects/level-07/update.js
+++ lesson-04-js-objects/level-07/update.js
@@ -1,4 +1,12 @@
-// TODO: Update existing properties and export the updated object.
-const obj = { age: 0 };
-// TODO: update obj.age
-export default obj;
+const student = {
+  level: 2,
+  firstname: "Rolazar",
+  lastname: "Aberin",
+};
+
+student.level = 1;
+student.firstname = "Bob";
+console.log("The student level after : " + student.level);
+console.log("The student name after : " + student.firstname);
+
+export default student;
