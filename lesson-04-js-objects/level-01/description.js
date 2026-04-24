ceaa85b26ecbabe1cc662ff8660b1aec9f7cfcca
Rolazar <rolazarcoding@gmail.com>
2026-04-23 17:04:42 -0700

    finished level 1

--- lesson-04-js-objects/level-01/description.js
+++ lesson-04-js-objects/level-01/description.js
@@ -1,3 +1,3 @@
 // TODO: Default-export a short description string about objects.
 // Example: export default "Objects are key/value collections."
-export default "TODO: A short description of objects.";
+export default "JavaScript objects can contain different types of data, like an array. But instead of accessing the data with an index number, data in JavaScript objects is accessed by name.";
--- lesson-04-js-objects/level-01/examples.js
+++ lesson-04-js-objects/level-01/examples.js
@@ -1,3 +1,6 @@
 // TODO: Default-export an array of two short example strings.
 // Example: export default ["{ a: 1 }", "{ name: 'Ava' }"]
-export default ["TODO example 1", "TODO example 2"];
+export default [
+  "JavaScript objects can contain different types of data, like an array.",
+  "But instead of accessing the data with an index number, data in JavaScript objects is accessed by name.",
+];
