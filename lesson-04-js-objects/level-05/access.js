1bba24f6142821edbc023af6bb32f906595b5cc4
Rolazar <rolazarcoding@gmail.com>
2026-04-23 17:30:38 -0700

    finished level 5

--- lesson-04-js-objects/level-05/access.js
+++ lesson-04-js-objects/level-05/access.js
@@ -1,3 +1,16 @@
 // TODO: Demonstrate dot-notation property access and default-export the result/example.
 // Example: const user = { name: 'Ava' }; export default user.name;
-export default null;
+
+const letters = ["a", "b", "c"];
+const item = letters[0];
+// console.log(item);
+
+const student = {
+  level: 2,
+  firstname: "Rolazar",
+  lastname: "Aberin",
+};
+const item2 = student.level;
+console.log(item2);
+
+export default student.level;
