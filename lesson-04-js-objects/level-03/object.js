8855571d93b9ea3c861d24b8a108ed8f4a0bf8f8
Rolazar <rolazarcoding@gmail.com>
2026-04-23 17:18:12 -0700

    finished level 3

--- lesson-04-js-objects/level-03/object.js
+++ lesson-04-js-objects/level-03/object.js
@@ -1,6 +1,2 @@
-// TODO: Declare a simple object and export it as the default export.
-// Example: const user = { name: 'Ava' }; export default user;
-const studentObject = {
-  /* TODO: add properties */
-};
-export default studentObject;
+const student = { level: 2 };
+export default student;
