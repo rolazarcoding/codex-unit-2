f10128e3f87b559a16a3511fda4b75208089593e
Rolazar <rolazarcoding@gmail.com>
2026-04-23 18:12:42 -0700

    finished level 8

--- lesson-04-js-objects/level-08/nested.js
+++ lesson-04-js-objects/level-08/nested.js
@@ -1,5 +1,17 @@
-// TODO: Default-export an object with nested properties.
-// Example: export default { theme: { color: 'blue' } }
-export default {
-  /* TODO: nested structure */
+const car = {
+  year: 2026,
+  make: "Ford",
+  model: "F150",
+  features: {
+    autoIdle: true,
+    antiLock: true,
+    bluetooth: true,
+    diesel: false,
+    seats: 5,
+    doors: 4,
+  },
 };
+
+console.log(car.features.doors);
+
+export default car;
