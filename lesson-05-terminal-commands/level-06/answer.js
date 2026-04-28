13d34586e73f668ad4cf45a0e50a20d85b11f0b8
Rolazar <rolazarcoding@gmail.com>
2026-04-27 17:37:37 -0700

    finished level 6

--- lesson-05-terminal-commands/level-06/answer.js
+++ lesson-05-terminal-commands/level-06/answer.js
@@ -1,3 +1,3 @@
 // Level 06 — Run Node.js files with node
 // TODO: Run `node hello.js` and write a short note here mentioning `node`.
-export default "";
+export default "The node command runs a specified JavaScript file.";
