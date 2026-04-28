1da78204c7b833b7fbb54645d58ae8ae587104f7
Rolazar <rolazarcoding@gmail.com>
2026-04-27 17:29:15 -0700

    finished level 4

--- lesson-05-terminal-commands/level-04/answer.js
+++ lesson-05-terminal-commands/level-04/answer.js
@@ -1,3 +1,3 @@
 // Level 04 — Current directory (.)
 // TODO: Run a script using `./` and write a short note here mentioning `.` or `./`.
-export default "";
+export default "The . represents the current folder.";
