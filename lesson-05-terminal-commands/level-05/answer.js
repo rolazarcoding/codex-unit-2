75b7ec35de16070d60d50aa387b8261405fa8395
Rolazar <rolazarcoding@gmail.com>
2026-04-27 17:32:11 -0700

    finished level 5

--- lesson-05-terminal-commands/level-05/answer.js
+++ lesson-05-terminal-commands/level-05/answer.js
@@ -1,3 +1,3 @@
 // Level 05 — Parent directory (..)
 // TODO: Use `cd ..` and/or `../` to reference sibling files; describe the steps here.
-export default "";
+export default "The .. represents the parent folder. I can move to the parent folder with cd ..";
