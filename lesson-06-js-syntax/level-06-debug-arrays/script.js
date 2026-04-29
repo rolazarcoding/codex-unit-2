e332aad40ac2769f6e94e3f32080843e787e8e94
Rolazar <rolazarcoding@gmail.com>
2026-04-28 18:00:45 -0700

    paritally finished level 6

--- lesson-06-js-syntax/level-06-debug-arrays/script.js
+++ lesson-06-js-syntax/level-06-debug-arrays/script.js
@@ -1,7 +1,7 @@
 // Level 06 — Debug array declarations
 // TODO: Fix provided broken array snippets so each is a valid one-line array with >=3 items.
 
-const arr1 = "a", "b", "c";
+const arr1 = ["a", "b", "c"];
 
 const arr2 = ["a" "b", "c"];
 
@@ -11,10 +11,9 @@ const arr4 = [apple, banana, cherry];
 
 const = [1,2,3];
 
-const arr6 = [1,2,
-3]
+const arr6 = [1,2,3];
 
-const arr7 = ['a", 'b', 'c'];
+const arr7 = ["a", "b", "c"];
 
 const arr8 = [1,2,3,];
 
