0f1cf4eab641be4e6dbcfb0b0ae8d7bcca614241
Rolazar <rolazarcoding@gmail.com>
2026-04-30 17:27:29 -0700

    finished level 2

--- lesson-08-rest-api/level-02/script.js
+++ lesson-08-rest-api/level-02/script.js
@@ -1,11 +1,11 @@
 // Level 02 starter script
 // TODO: Get & save the form element to `form`, attach onsubmit, and call event.preventDefault().
 const form = document.getElementById("sample-form");
-const result = document.getElementById("result");
+form.onsubmit = handleSubmit;
 
 function handleSubmit(event) {
   event.preventDefault();
-  // TODO: set result.innerText to a short explanation about fetch
+  const resultTag = document.getElementById("result");
+  resultTag.innerText =
+    "The fetch function takes in a URL and communicates with a server.";
 }
-
-if (form) form.onsubmit = handleSubmit;
